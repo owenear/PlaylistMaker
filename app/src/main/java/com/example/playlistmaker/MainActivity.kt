dac0ac968570c85b72886171299244f1e07ca50c
owenear <owenear@gmail.com>
2024-06-12 15:28:23 +0300

    toasts done

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val libraryButton = findViewById<Button>(R.id.library_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        val buttonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+            Toast.makeText(this@MainActivity, "Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        searchButton.setOnClickListener(buttonClickListener)
+
+        libraryButton.setOnClickListener{ Toast.makeText(this@MainActivity, "Медиатека", Toast.LENGTH_SHORT).show() }
+
+        settingsButton.setOnClickListener{ Toast.makeText(this@MainActivity, "Настройки", Toast.LENGTH_SHORT).show() }
+
     }
 }
